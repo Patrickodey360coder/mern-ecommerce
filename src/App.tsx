5b8e9c9b550c7502de258be71d07b52a151b1331
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-31 07:01:44 -0500

    navbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import './Navbar.css';
+import logo from '../../assets/logo.png'
+import cart_icon from '../../assets/cart_icon.png'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>Ecommerce</p>
+      </div>
+
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
