bf0e5e382c61499739f7202ee73ca070f101f5fa
Patrickodey360coder <patrickodey419@gmail.com>
2024-07-31 07:32:28 -0500

    added state management to navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import './Navbar.css';
 import logo from '../../assets/logo.png'
 import cart_icon from '../../assets/cart_icon.png'
+import { useState } from 'react';
 
 
 const Navbar = () => {
+  const [ menu, setMenu] = useState<string>("shop")
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -12,15 +14,16 @@ const Navbar = () => {
       </div>
 
       <ul className='nav-menu'>
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => {setMenu("shop")}}>Shop { menu === "shop" ? <hr /> : <></> }</li>
+        <li onClick={() => {setMenu("men")}}>Men { menu === "men" ? <hr /> : <></> }</li>
+        <li onClick={() => {setMenu("women")}}>Women { menu === "women" ? <hr /> : <></> }</li>
+        <li onClick={() => {setMenu("kids")}}>Kids { menu === "kids" ? <hr /> : <></> }</li>
       </ul>
 
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="cart" />
+        <div className='nav-cart-count'>0</div>
       </div>
     </div>
   )
