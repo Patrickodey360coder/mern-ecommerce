3f8e03f622f6822495ade1d9624dec794a272ec6
Patrickodey360coder <patrickodey419@gmail.com>
2024-08-05 15:20:50 +0100

    New collections

--- src/Components/NewCollections/NewCollections.tsx
+++ src/Components/NewCollections/NewCollections.tsx
@@ -1,8 +1,28 @@
 import './NewCollections.css'
+import new_collection from '../../assets/new_collections.js'
+import Item from '../Items/Items'
+
+type Product = {
+  id: number;
+  name: string;
+  image: string;
+  new_price: string;
+  old_price: string
+}
 
 const NewCollections = () => {
   return (
-    <div>NewCollections</div>
+    <div className='new-collections'>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {
+          new_collection.map((item: Product, id: number) => {
+            return <Item  key={id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          })
+        }
+      </div>
+    </div>
   )
 }
 
