d59ca0e7cc8840005aed0af85b957365e45ac7ff
Patrickodey360coder <patrickodey419@gmail.com>
2024-08-07 23:39:16 +0100

    newsletter

--- src/Components/Newsletter/Newsletter.tsx
+++ src/Components/Newsletter/Newsletter.tsx
@@ -2,7 +2,15 @@ import './Newsetter.css'
 
 const Newsletter = () => {
   return (
-    <div className='newsletter'>Newsletter</div>
+    <div className='newsletter'>
+      <h1>Get Exclusive Offers on Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+
+      <div>
+        <input type="email" placeholder='Your Email id'/>
+        <button>Subscribe</button>
+      </div>
+    </div>
   )
 }
 
